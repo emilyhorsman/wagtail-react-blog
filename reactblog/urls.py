44d0d0db5e6b3e67f2100d51943b74553b1ddbfb
Emily Horsman <me@emilyhorsman.com>
2016-05-12 23:46:50 -0400

    Fix urls, should always include wagtail_urls

--- reactblog/urls.py
+++ reactblog/urls.py
@@ -6,6 +6,7 @@ from django.views.generic import TemplateView
 from wagtail.wagtailadmin import urls as wagtailadmin_urls
 from wagtail.wagtaildocs import urls as wagtaildocs_urls
 from wagtail.wagtailcore import urls as wagtail_urls
+import wagtail.wagtailimages.views
 from wagtail.contrib.wagtailapi import urls as wagtailapi_urls
 
 from search import views as search_views
@@ -20,6 +21,7 @@ urlpatterns = [
     url(r'^search/$', search_views.search, name='search'),
 
     url(r'^api/', include(wagtailapi_urls)),
+    url(r'^wagtail/', include(wagtail_urls)),
 ]
 
 
